19fd7fc8eeb30b31e0f7d998b180e31df98bee5d
louchu0604 <louchu0604@gmail.com>
2018-10-31 22:57:30 +0800

    add tab sel

--- PieDemo/PieDemo/CYTracking/UITabBarController+CYPush.m
+++ PieDemo/PieDemo/CYTracking/UITabBarController+CYPush.m
@@ -16,9 +16,30 @@
         Method newMethod = class_getInstanceMethod([self class], @selector(p_cy_tabBarController:didSelectViewController:));
         method_exchangeImplementations(originMethod,newMethod);
     }
+    {
+        Method originMethod = class_getInstanceMethod([self class], @selector(setSelectedIndex:));
+        Method newMethod = class_getInstanceMethod([self class], @selector(p_cy_setSelectedIndex:));
+        method_exchangeImplementations(originMethod,newMethod);
+    }
+    {
+        Method originMethod = class_getInstanceMethod([self class], @selector(_tabBarItemClicked:));
+        Method newMethod = class_getInstanceMethod([self class], @selector(p_cy_tabBarItemClicked:));
+        method_exchangeImplementations(originMethod,newMethod);
+    }
+}
+- (void)p_cy_tabBarItemClicked:(UIBarButtonItem *)item
+{
+    NSLog(@"user select:%@",item.title);
+    [self p_cy_tabBarItemClicked:item];
+}
+- (void)p_cy_setSelectedIndex:(NSUInteger)selectedIndex
+{
+    NSLog(@"cytrace== tabSelectedIndex:%lu",(unsigned long)selectedIndex);
+    [self p_cy_setSelectedIndex:selectedIndex];
+    
 }
 - (void)p_cy_tabBarController:(UITabBarController *)tabBarController didSelectViewController:(UIViewController *)viewController{
-    
+    NSLog(@"cytrace== tab:%@  select:%@",NSStringFromClass([tabBarController class]),NSStringFromClass([viewController class]));
     [self p_cy_tabBarController:tabBarController didSelectViewController:viewController];
     
 }
