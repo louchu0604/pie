bcbe3c45b1e96227a0c4936f2a61746878b5e384
louchu <louchu0604@gmail.com>
2018-11-03 23:37:41 +0800

    urls

---
+++ PieDemo/PieDemo/CYUIKit/CYFPSLabel.h
@@ -0,0 +1,17 @@
+//
+//  CYFPSLabel.h
+//  PieDemo
+//
+//  Created by louchu on 2018/11/2.
+//  Copyright © 2018年 Cy Lou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CYFPSLabel : UILabel
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ PieDemo/PieDemo/CYUIKit/CYFPSLabel.m
@@ -0,0 +1,21 @@
+//
+//  CYFPSLabel.m
+//  PieDemo
+//
+//  Created by louchu on 2018/11/2.
+//  Copyright © 2018年 Cy Lou. All rights reserved.
+//
+
+#import "CYFPSLabel.h"
+
+@implementation CYFPSLabel
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
